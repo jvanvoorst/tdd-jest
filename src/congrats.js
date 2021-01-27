515252821d5b7e103ecba6b4adc0fa216f77dd6c
Justin <justin.vanvoorst@motili.com>
2021-01-27 15:22:35 -0700

    add styling

--- src/congrats.js
+++ src/congrats.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 export default function Congrats({ success }) {
   if (success) {
     return (
-      <div data-test="component-congrats">
+      <div data-test="component-congrats" className="alert alert-success">
         <span data-test="congrats-message">
-          Congratulations You guessed the word"
+          Congratulations You guessed the word
         </span>
       </div>
     );
