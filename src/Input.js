81cc9cbae5224971f8c53c2ce1bd02243b0a78d3
Justin <justin.vanvoorst@motili.com>
2021-01-28 14:30:25 -0700

    input tests

--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 class Input extends Component {
   render() {
-    return <div>Input</div>;
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          className="mb-3 mx-sm-3"
+          type="text"
+          placeholder="enter guess"
+        />
+        <button
+          data-test="submit-button"
+          className="btn btn-primary mb-2"
+          type="submit"
+        >
+          Submit
+        </button>
+      </form>
+    );
+    return <div data-test="component-input">{contents}</div>;
   }
 }
-Input.propTypes = {};
 
-const mapStateToProps = (state) => {
-  return {};
+const mapStateToProps = ({ success }) => {
+  return { success };
 };
 
 export default connect(mapStateToProps)(Input);
