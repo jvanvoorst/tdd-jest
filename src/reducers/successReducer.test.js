0a7d9a8d05dfa0c8a31101aa04aeb596a9462975
Justin <justin.vanvoorst@motili.com>
2021-01-28 13:13:54 -0700

    success reducer test

---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,14 @@
+import { actionTypes } from "../actions";
+import successReducer from "./successReducer";
+
+test("returns default initial state of false when no action has been taken", () => {
+  const newState = successReducer(undefined, {});
+  expect(newState).toBe(false);
+});
+
+test("returns state of true upon recieving action type CORRECT_GUESS", () => {
+  const newState = successReducer(undefined, {
+    type: actionTypes.CORRECT_GUESS,
+  });
+  expect(newState).toBe(true);
+});
