9b8af68bef1a62053dd15bc18a6d326534a3d301
Justin <justin.vanvoorst@motili.com>
2021-01-28 17:33:59 -0700

    getSecretWord action creator tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -1,10 +1,31 @@
-import { correctGuess, actionTypes } from "./";
+import moxios from "moxios";
 
-// describe("correctGuess", () => {
-//   test("returns an action with type CORRECT_GUESS", () => {
-//     const action = correctGuess();
-//     expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-//   });
-// });
+import { storeFactory } from "../../test/testUtils";
+import { getSecretWord } from "./index";
 
-test("placeholder", () => {});
+describe("getSecretWord action creator", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test("adds response word to state", () => {
+    const secretWord = "party";
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      });
+    });
+
+    return store.dispatch(getSecretWord()).then(() => {
+      const actual = store.getState().secretWord;
+      expect(actual).toBe(secretWord);
+    });
+  });
+});
